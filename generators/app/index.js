d13a418395e12e02de3fdcc209d7ee55595ba088
聂开发 <niekaifa@apowo.com>
2020-11-23 18:26:30 +0800

    init

---
+++ generators/app/index.js
@@ -0,0 +1,45 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator{
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initPackage() {
+    const pkgJson = {
+      devDependencies: {
+        eslint: '^3.15.0'
+      },
+      dependencies: {
+        react: '^16.2.0'
+      }
+    };
+
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+    this.npmInstall();
+  } 
+
+  async method1() {
+    // const answers = await this.prompt([
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "Your project name",
+    //     default: this.appname
+    //   },
+    //   {
+    //     type: 'confirm',
+    //     name: 'cool',
+    //     message: 'Would you like to enable the Cool Feature?'
+    //   }
+    // ]);
+    // this.log('app name', answers.name);
+    // this.log('cool feature', answers.cool);
+
+    this.fs.copyTpl(
+      this.templatePath('t.html'),
+      this.destinationPath('public/index.html'),
+      { title: 'Templating with Yeoman' }
+    )
+  }
+};
